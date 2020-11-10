a1c32db24f992f80b32ad2bc9c9e75002bc9e850
Lucas Stumpf <74257033+Lustumpf@users.noreply.github.com>
2020-11-10 13:24:48 -0300

    Add files via upload

---
+++ vampire2.py
@@ -0,0 +1,6 @@
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, grannie.')
+elif age > 2000:
+    print('Unlike you, Alice is not and undead, immortal vampire.')
